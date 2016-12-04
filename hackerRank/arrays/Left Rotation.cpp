eb7abab1576a5404abf90c9e1265af40387fad8b
liamgall <liamgall@naver.com>
2016-12-04 21:59:19 +0900

    arrays // C++ // left rotation.cpp

--- hackerRank/arrays/Left Rotation.cpp
+++ hackerRank/arrays/Left Rotation.cpp
@@ -7,6 +7,15 @@ using namespace std;
 
 
 int main() {
-    
-    return 0;
+	int n, d;
+	cin >> n >> d;
+	int ptr = d;
+	vector <int> arr(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n; ++i) {
+		cout << arr[ptr++%n] << " ";
+	}
+	return 0;
 }
