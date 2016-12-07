59219fd3976cf23e24d0bebc253546af000b9b5c
liamgall <liamgall@naver.com>
2016-12-07 23:57:46 +0900

    mergelists

---
+++ hackerRank/Linked Lists/Merge two sorted linked lists.cpp
@@ -0,0 +1,14 @@
+/*
+  Merge two sorted lists A and B as one linked list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* MergeLists(Node *headA, Node* headB)
+{
+  // This is a "method-only" submission.
+  // You only need to complete this method
+}
