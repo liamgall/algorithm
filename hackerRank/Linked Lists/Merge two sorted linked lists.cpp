44ab136686984dd4a7b4c39e4966096318a89b1f
liamgall <liamgall@naver.com>
2016-12-10 23:23:53 +0900

    d

--- hackerRank/Linked Lists/Merge two sorted linked lists.cpp
+++ hackerRank/Linked Lists/Merge two sorted linked lists.cpp
@@ -1,24 +1,18 @@
 /*
-Merge two sorted lists A and B as one linked list
-Node is defined as
-struct Node
-{
-int data;
-struct Node *next;
-}
+  Get Nth element from the end in a linked list of integers
+  Number of elements in the list will always be greater than N.
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
 */
-Node* MergeLists(Node *headA, Node* headB)
+int GetNode(Node *head,int positionFromTail)
 {
-	if (!headA)
-		return headB;
-	if (!headB)
-		return headA;
-	if (headA->data > headB->data) {
-		headB->next = MergeLists(headA, headB->next);
-		return headB;
-	}
-	else {
-		headA->next = MergeLists(headA->next, headB);
-		return headA;
-	}
+  // This is a "method-only" submission.
+  // You only need to complete this method.
+
+  if(head == NULL)
+  return 0;
 }
