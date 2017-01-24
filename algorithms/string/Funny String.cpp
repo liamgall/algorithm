4175647e8e785eb289f52bb4392bc6aa2064c9ed
liamgall <liamgall@naver.com>
2017-01-24 14:53:59 +0900

    funny string

---
+++ algorithms/string/Funny String.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int checkStr(char* str) {
+	int length = strlen(str);
+	for (int i = 1; i < length; ++i) {
+		if (abs(str[i] - str[i - 1]) != abs(str[length - i] - str[length - i - 1])) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+int main(void) {
+	char str[10001];
+	int testCase;
+	cin >> testCase;
+	while (testCase--) {
+		memset(str, 0, sizeof(str));
+		cin >> str;
+
+		if (checkStr(str) == 0) {
+			cout << "Funny" << endl;
+		}
+		else {
+			cout << "Not Funny" << endl;
+		}
+	}
+	return 0;
+}
